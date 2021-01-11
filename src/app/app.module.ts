c7a676fa2ec91d5bd21e159f46a4c6940f98974f
Sylvia <sylviasastre@gmail.com>
2021-01-11 02:30:02 +0100

    Refactor module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { Injector } from '@angular/core';
-import { createCustomElement } from '@angular/elements';
-
 import { Exercise1Component } from './components/exercise1/exercise1.component';
 import { Exercise2Component } from './components/exercise2/exercise2.component';
-
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { FormsModule } from '@angular/forms';
 import { CustomRangeModule } from './components/custom-range/custom-range.module';
-
-import { HttpClient, HttpClientModule, HttpResponse } from '@angular/common/http';
-
+import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
