68041a35361aac6ccbeade5865300a29ab275b89
Sylvia <sylviasastre@gmail.com>
2021-01-15 19:00:58 +0100

    Add events

--- src/app/components/custom-range/custom-range.component.ts
+++ src/app/components/custom-range/custom-range.component.ts
@@ -1,135 +1,202 @@
 import { CdkDragEnd } from '@angular/cdk/drag-drop';
-import { Input } from '@angular/core';
-import { Component, OnInit, forwardRef } from '@angular/core';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
+import { ChangeDetectorRef, Input, Output} from '@angular/core';
+import { Component, OnInit, forwardRef, EventEmitter } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, Validators } from '@angular/forms';
+import { PriceRange } from 'src/app/models/PriceRange.model';
+import { FormGroup, FormControl } from '@angular/forms';
 
 export const CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR: any = {
-  provide: NG_VALUE_ACCESSOR,
-  useExisting: forwardRef(() => CustomRangeComponent),
-  multi: true
-};
-
-@Component({
-  // tslint:disable-next-line: component-selector
-  selector: 'ngc-range',
-  templateUrl: './custom-range.component.html',
-  styleUrls: ['./custom-range.component.scss'],
-  providers: [{
     provide: NG_VALUE_ACCESSOR,
     useExisting: forwardRef(() => CustomRangeComponent),
     multi: true
-  }]
+};
+
+@Component({
+    // tslint:disable-next-line: component-selector
+    selector: 'ngc-range',
+    templateUrl: './custom-range.component.html',
+    styleUrls: ['./custom-range.component.scss'],
+    providers: [{
+        provide: NG_VALUE_ACCESSOR,
+        useExisting: forwardRef(() => CustomRangeComponent),
+        multi: true
+    }]
 })
 export class CustomRangeComponent implements OnInit, ControlValueAccessor {
+    @Input() fixed = false;
+    @Input() minValue: number;
+    @Input() maxValue: number;
+    @Output() rangeChange = new EventEmitter();
 
-  @Input() fixed = false;
-  @Input() minValue: any;
-  @Input() maxValue: any;
+    form: FormGroup;
+    priceRange: PriceRange = {min: 0, max: 100};
 
-  firstBulletDragPosition  = {x: 0, y: 0};
-  secondBulletDragPosition = {x: 200, y: 0};
+    // Bullets coordenates
+    firstBulletDragPosition = { x: 0, y: 0 };
+    secondBulletDragPosition = { x: 200, y: 0 };
 
-  firstInputActive = false;
-  secondInputActive = false;
+    // Is input form active or else show label
+    firstInputActive = false;
+    secondInputActive = false;
 
-  firstValue = 0;
-  secondValue = 200;
+    // Axis limit values in px
+    minPxValue = 0;
+    maxPxValue = 200;
 
-  firstBoundaryWidth     = '188px';
-  secondBoundaryWidth    = 200;
-  secondBoundaryPosition = '0px';
+    // Boundaries for bullet positions
+    firstBoundaryWidth = 200;
+    secondBoundaryWidth = 200;
+    secondBoundaryPosition = 0;
 
-  constructor() {
-  }
-  writeValue(obj: any): void {
-    // throw new Error('Method not implemented.');
-  }
-  registerOnChange(fn: any): void {
-    // throw new Error('Method not implemented.');
-  }
-  registerOnTouched(fn: any): void {
-    // throw new Error('Method not implemented.');
-  }
+    pricePerPx = 0;
 
-  ngOnInit(): void {
-    if (this.maxValue === undefined) {
-      this.maxValue = 300;
+    constructor(private detectChange: ChangeDetectorRef) {
+        this.minValue = 0;
+        this.maxValue = 10;
+        this.priceRange = new PriceRange();
+        this.form = new FormGroup({
+            firstValueInput: new FormControl('', [Validators.minLength(0)]),
+            secondValueInput: new FormControl('', [Validators.maxLength(1000)])
+        });
     }
-    // const firstPointer = document.querySelector('#firstPointer') as HTMLInputElement;
-    // const secondPointer = document.querySelector('#secondPointer') as HTMLInputElement;
-    const input1 = document.querySelector('#firstValueInput') as HTMLInputElement;
-    // firstPointer.style.transform = 'translate3d(0px, 0px, 0px)';
-    // secondPointer.style.transform = 'translate3d(0px, 0px, 0px)';
-    window.addEventListener('click', (aux) => {
-      console.log(aux.currentTarget);
-      if (input1 !== null) {
-        if (this.firstInputActive === true && !input1.hasAttribute('focus')){
-          this.inputActive(true);
+
+    onChange: (_: any) => void = (_: any) => {};
+    onTouched: () => void = () => {};
+    updateChanges(): void {
+        this.onChange(this.priceRange);
+    }
+    writeValue(priceRange: PriceRange): void {
+        this.priceRange = priceRange;
+        this.updateChanges();
+    }
+    registerOnChange(fn: any): void {
+        this.onChange = fn;
+    }
+    registerOnTouched(fn: any): void {
+        this.onTouched = fn;
+    }
+
+    ngOnInit(): void {}
+
+    /**
+     * Given a price value from input, change corresponding bullet position.
+     * @param inputValue Price value from input.
+     * @param isFirstBullet Check if is relative to minValue or maxValue.
+     */
+    changeBulletPosition(inputValue: string, isFirstBullet: boolean): void {
+        if (isFirstBullet) {
+            if ( (parseInt(inputValue, 0) >= this.minValue) && this.maxValue > parseInt(inputValue, 0)) {
+                const position = this.getProportion(this.maxPxValue, parseInt(inputValue, 0), (this.maxValue - this.minValue));
+                this.firstBulletDragPosition = { x: position, y: 0 };
+                this.secondBoundaryWidth = (this.maxPxValue - position) - 12;
+                this.secondBoundaryPosition = position + 12;
+                this.secondBulletDragPosition = { x: (200 - position) - 12, y: 0 };
+            } else {
+                this.form.controls.firstValueInput.setErrors({ invalid: true });
+            }
+        } else {
+            if ( (parseInt(inputValue, 0) <= this.maxValue) && this.minValue < parseInt(inputValue, 0)) {
+                const auxW = this.secondBoundaryWidth;
+                const auxM = this.secondBoundaryPosition;
+                this.secondBoundaryWidth = 0;
+                this.secondBoundaryPosition = 0;
+                const position = this.getProportion(this.maxPxValue, parseInt(inputValue, 0), (this.maxValue - this.minValue));
+                this.secondBulletDragPosition = { x: position, y: 0 };
+                this.firstBoundaryWidth = position + auxM;
+                this.secondBoundaryWidth = auxW;
+                this.secondBoundaryPosition = auxM;
+            } else {
+                this.form.controls.secondValueInput.setErrors({ invalid: true });
+            }
+            // calculate fisrt boundary width;
         }
-      }
-    });
-  }
-
-  changeBulletPosition(inputValue: string, isFirstBullet: boolean): void {
-    // tslint:disable-next-line: radix
-    if (isFirstBullet) {
-      // tslint:disable-next-line: radix
-      this.firstBulletDragPosition = {x: parseInt(inputValue), y: 0};
-    } else {
-      // tslint:disable-next-line: radix
-      this.secondBulletDragPosition = {x: parseInt(inputValue), y: 0};
     }
-  }
 
-  getFirstBulletValue(event: CdkDragEnd): void {
-    console.log('first bullet position' + event.source.getFreeDragPosition().x);
-    // this.firstValue = this.getPercentage(200, event.source.getFreeDragPosition().x);
-    this.firstValue = event.source.getFreeDragPosition().x;
-    // this.secondBoundaryWidth = this.getSecondBoundaryWidth();
-    // this.secondBoundaryPosition = this.getSecondBoundaryPosition();
-  }
-
-  getSecondBulletValue(event: CdkDragEnd): void {
-    console.log('first bullet position ' + event.source.getFreeDragPosition().x);
-    this.secondValue = event.source.getFreeDragPosition().x;
-  }
-
-  /**
-   * @returns Position desired in percentage using a rule of three.
-   * @param valueSelected bullet axis position in px.
-   * @param maxValue position from the second bullet; initially the total width from the range
-   * div - 12px which is the size from the bullet.
-   */
-  getPercentage(maxValue: number, position: number): number {
-    console.log('maxValue:' + maxValue, 'position: ' + position );
-    return (position / 100) * maxValue;
-  }
-
-  inputActive(isFirstInput: boolean): void{
-    if (isFirstInput) {
-      this.firstInputActive === false ? this.firstInputActive = true : this.firstInputActive = false;
-    } else {
-      this.secondInputActive === false ? this.secondInputActive = true : this.secondInputActive = false;
+    /**
+     * Given a price value calculate px proportion using a rule of three.
+     * @returns px position.
+     * @param x Relative value.
+     * @param selectedValue Input value.
+     * @param maxValue Total price range.
+     */
+    getProportion(x: number, selectedValue: number, maxValue: number): number {
+        return  Math.floor((selectedValue * x) / maxValue);
     }
-  }
 
-  getFirstBoundaryWidth(): string{
-    console.log('sec.value' + this.secondValue);
-    const auxValue = this.secondValue < 88 ? this.secondValue + 12 : this.secondValue + 12;
-    return (auxValue).toString() + 'px';
-  }
+    /**
+     * Get relation price per px.
+     * E.g. (300€ as max price - 5€ as min price) / 200px as axis total width
+     */
+    getPricePerPx(): number {
+        const rangeTotal = this.maxValue - this.minValue;
+        return rangeTotal / this.maxPxValue;
+    }
 
-  getSecondBoundaryWidth(): void{
-    const num = this.firstValue > 12 ? (this.maxValue - this.firstValue) : (this.maxValue - this.firstValue);
-    this.secondBoundaryWidth = num;
-    this.secondBulletDragPosition = {x: this.getRelativePosition(), y: this.secondBulletDragPosition.y}; }
+    /**
+     * Calculate price by given position.
+     * @param position Bullet position.
+     */
+    getRelativePrice(position: number, margin?: number): number {
+        const pricePerPx = this.getPricePerPx();
+        let calc = 0;
+        if (margin) {
+            const total = position + margin;
+            calc = Math.floor((total * pricePerPx) + this.minValue);
+        } else {
+            calc = Math.floor((position * pricePerPx) + this.minValue);
+        }
+        return calc;
+    }
 
-  getSecondBoundaryPosition(): void {
-    this.secondBoundaryPosition = (this.firstValue).toString() + 'px';
-  }
+    /**
+     * Sets min price by bullet position value.
+     * @param event Event emitted when the user stops dragging a draggable.
+     */
+    getFirstBulletValue(event: CdkDragEnd): void {
+        this.priceRange.min = this.getRelativePrice(event.source.getFreeDragPosition().x);
+        this.firstBulletDragPosition.x = event.source.getFreeDragPosition().x;
+        const position = this.getProportion(this.maxPxValue, this.priceRange.min, (this.maxValue - this.minValue));
+        console.log(position);
+        this.secondBoundaryPosition = this.firstBulletDragPosition.x + 12;
+        this.secondBoundaryWidth = (this.maxPxValue - this.secondBoundaryPosition);
+        this.secondBulletDragPosition = { x: (this.secondBulletDragPosition.x - this.secondBoundaryPosition), y: 0 };
+        this.rangeChange.emit(this.priceRange);
+    }
 
-  getRelativePosition(): number {
-    return (this.secondBulletDragPosition.x / 200) * this.secondBoundaryWidth;
-  }
+    getSecondBulletValue(event: CdkDragEnd): void {
+        this.priceRange.max = this.getRelativePrice(event.source.getFreeDragPosition().x, this.secondBoundaryPosition);
+        this.secondBulletDragPosition.x = event.source.getFreeDragPosition().x;
+        this.rangeChange.emit(this.priceRange);
+    }
+
+    /**
+     * Switches inputs to active or inactive in order to display price label or input form.
+     */
+    inputActive(isFirstInput: boolean): void {
+        if (isFirstInput) {
+            this.firstInputActive === false ? this.firstInputActive = true : this.firstInputActive = false;
+        } else {
+            this.secondInputActive === false ? this.secondInputActive = true : this.secondInputActive = false;
+        }
+    }
+
+    getFirstBoundaryWidth(): string {
+        // const auxValue = this.secondValue < 188 ? this.secondValue + 12 : this.secondValue - 12;
+        return (this.maxPxValue).toString() + 'px';
+    }
+
+    getSecondBoundaryWidth(): void {
+        const num = this.minPxValue > 12 ? (this.maxValue - this.minPxValue) : (this.maxValue - this.minPxValue);
+        this.secondBoundaryWidth = num;
+        // this.secondBulletDragPosition = { x: this.getRelativePosition(), y: this.secondBulletDragPosition.y };
+    }
+
+    getSecondBoundaryPosition(): void {
+        //this.secondBoundaryPosition = (this.minPxValue).toString() + 'px';
+    }
+
+    // getRelativePosition(): number {
+    //     return (this.secondBulletDragPosition.x / 200) * this.secondBoundaryWidth;
+    // }
 
 }
--- src/app/components/exercise1/exercise1.component.ts
+++ src/app/components/exercise1/exercise1.component.ts
@@ -1,31 +1,34 @@
-import { CdkDragEnd } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
-import { throwError } from 'rxjs';
 import { PriceRange } from 'src/app/models/PriceRange.model';
 import { PricesService } from 'src/app/services/prices.service';
 
 @Component({
-  selector: 'custom-range-exercise1',
-  templateUrl: './exercise1.component.html',
-  styleUrls: ['./exercise1.component.scss']
+    selector: 'custom-range-exercise1',
+    templateUrl: './exercise1.component.html',
+    styleUrls: ['./exercise1.component.scss']
 })
 export class Exercise1Component implements OnInit {
-  min = 0;
-  max = 100;
-  priceRange: PriceRange = new PriceRange();
+    priceRange: PriceRange = new PriceRange();
+    minPrice = 0;
+    maxPrice = 0;
 
-  constructor(private priceService: PricesService) {
-    this.min = 0;
-    this.max = 200;
-    this.priceRange = new PriceRange();
-  }
-  ngOnInit(): void {
-    this.priceService.getRangePrices().subscribe(res => {
-      if (res.body !== null) {
-        this.priceRange = res.body;
-      }
-    }, error => {
-      throw new Error('Error:' + error.status);
-    }, () => console.log(this.priceRange));
-  }
+    constructor(private priceService: PricesService) {
+        this.priceRange = new PriceRange();
+    }
+
+    ngOnInit(): void {
+        this.priceService.getRangePrices().subscribe(response => {
+            if (response.body !== null) {
+                this.priceRange = response.body;
+                this.minPrice = this.priceRange.min;
+                this.maxPrice = this.priceRange.max;
+            }
+        }, error => {
+            throw new Error('Error:' + error.status);
+        }, () => console.log(this.priceRange));
+    }
+
+    setPrices(event: PriceRange): void {
+        this.priceRange = event;
+    }
 }
